0a8eccef4911e843e329044b4d01b90fb5fd00f8
ariya <ariya_watchara@cmu.ac.th>
2022-09-17 10:46:25 +0700

    base file 2.0

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from './features/Navbar';
+import GlobalStyle from './features/GlobalStyle';
+
+const App = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <Navbar />
+        </>
+    )
+}
+
+export default App;
